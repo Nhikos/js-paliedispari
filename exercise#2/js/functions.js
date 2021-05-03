1154425815a5a566505a12c7b0b34a4123389b58
Nhikos <4ndre.sergi@gmail.com>
2021-05-03 18:08:08 +0200

    esercizio completato

--- exercise#2/js/functions.js
+++ exercise#2/js/functions.js
@@ -1,4 +1,12 @@
-// ESERCIZIO #1
-// 1 - Palindroma
-// Chiedere all'utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+function numeroCasuale() {
+    return Math.floor(Math.random() * 5) +1;
+}
+
+function sommaPari(somma) {
+    if (somma % 2 == 0){
+        console.log("la somma è pari");
+        return true;
+    } 
+    console.log("la somma è dispari");
+    return false;
+}
--- exercise#2/js/script.js
+++ exercise#2/js/script.js
@@ -6,4 +6,24 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-var ciawawe = prompt("gaaa");
+var scelta = prompt("Pari o Dispari?");
+var numeroScelto = parseInt(prompt("inserisci un numero da 1 a 5"));
+console.log("numero scelto da me ", numeroScelto);
+
+//funzione per generare un numero random
+var numeroCasuale = numeroCasuale()
+console.log("numero casuale ",numeroCasuale);
+
+// somma
+var somma = (numeroScelto + numeroCasuale);
+console.log("somma ",somma);
+
+// funzione per identificare se la somma dei numeri è pari
+var pari = sommaPari(somma);
+
+// condizione di vincita
+if ((scelta == "pari" && pari /* oppure pari == true */) || (scelta == "dispari" && !pari /* oppure pari == false */)) {
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
+}
